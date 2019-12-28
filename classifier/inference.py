bedf0b3f8117f46c832c8fef71746831134d5f30
pdoyle5000 <pdoyle5000@gmail.com>
2019-12-28 13:25:10 -0500

    created small inference class

---
+++ classifier/inference.py
@@ -0,0 +1,28 @@
+import torch
+from PIL.JpegImagePlugin import JpegImageFile
+from PIL.ImageStat import Stat
+import numpy as np
+from simple_net import SimpleNet
+from dataset import inference_composer
+
+
+class PneumoniaClassifier:
+    def __init__(self, model_path: str):
+        self.model = self._load_model(model_path)
+        self.model.eval()
+
+    def _load_model(self, model_path: str):
+        model = SimpleNet(1)
+        model.load_state_dict(torch.load(model_path))
+        return model
+
+    def predict(self, inputs: JpegImageFile) -> float:
+        stats = Stat(inputs)
+        composer = inference_composer(
+            (256, 256), np.divide(stats.stddev, 255), np.divide(stats.mean, 255)
+        )
+        with torch.no_grad():
+            image = composer(inputs)
+            output = self.model(image.float().unsqueeze(0))
+            sigmoid = torch.nn.Sigmoid()
+            return float(sigmoid(output.detach().squeeze(1)))
